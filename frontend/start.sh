553e61efa292950d6927d83bebe282d357c91d12
hedgty <hedgty@hedgty.me>
2020-07-07 23:18:17 +0000

    Add startup script

---
+++ frontend/start.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Bash Startup Script
+# Note: Requires Bash; Will not run in Sh
+
+
+#Process Command Line Options
+args=($@)
+j=0
+while [[ "${args[$j]}" != "" ]]
+do
+    case "${args[$j]}" in
+        -p | --port ) ((j++))
+                      port=${args[$j]}
+                      ;;
+        -s | --skip-install ) skip_install=true
+                              ;;
+        --help ) help=true
+                 ;;
+    esac
+    ((j++))
+done
+
+#Help Command
+if [[ "$help" == true ]]
+then
+    cat << EOM
+Usage: ./start.sh [OPTION]...
+Start the application.
+    -p, --port port     Set the application port.
+    -s, --skip-install  Skip installing dependencies.
+    --help              Display this help and exit.
+
+EOM
+    exit 0
+fi
+
+#Ask for port if not already set in options
+if [[ "$port" == "" ]]
+then
+    echo -n "Port:"
+    read port
+fi
+
+#Skip installing dependencies if set in options
+if [[ "$skip_install" != true ]]
+then
+npm install
+fi
+
+PORT=$port npm start
