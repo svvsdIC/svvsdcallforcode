03f2f55540e986b460fd15682596c474a42b3e20
hedgty <hedgty@hedgty.me>
2020-07-24 23:59:27 +0000

    Modify configure.sh

--- configure.sh
+++ configure.sh
@@ -14,6 +14,9 @@ read piapikey
 cat << EOF > .env
 PERSONALITY_INSIGHTS_URL=https://gateway.watsonplatform.net/personality-insights/api
 PERSONALITY_INSIGHTS_IAM_APIKEY=$piapikey
+
+TWITTER_CONSUMER_KEY=<twitter-consumer-key>
+TWITTER_CONSUMER_SECRET=<twitter-consumer-secret>
 EOF
 
 if [[ "$run" == "true" ]]
